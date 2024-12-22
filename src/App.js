cc7c74a38234b57e013068960e87ba2859a7851a
Gayathri G <gayathrigcse2022@gmail.com>
2024-12-22 22:29:33 +0530

    .

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Content from "./Content";
+import Header from "./Header";
+import Footer from "./Footer";
+import './index.css'
+
+function App() {
+  
+  return (
+    <div className = "App">
+      <Header />
+      <Content />
+      <Footer />
+    </div>//
+  );
+}
+
+export default App;
